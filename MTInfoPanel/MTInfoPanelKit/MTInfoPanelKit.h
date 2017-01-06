ccf9d0f1bb29d44129c01c3af0b30dc1f53f24a8
Juergen Falb <juergen@falb.at>
2017-01-06 22:29:48 +0100

    Added framework to project

---
+++ MTInfoPanel/MTInfoPanelKit/MTInfoPanelKit.h
@@ -0,0 +1,19 @@
+//
+//  MTInfoPanelKit.h
+//  MTInfoPanelKit
+//
+//  Created by Juergen Falb on 06/01/2017.
+//  Copyright © 2017 NOUS Wissensmanagement GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MTInfoPanelKit.
+FOUNDATION_EXPORT double MTInfoPanelKitVersionNumber;
+
+//! Project version string for MTInfoPanelKit.
+FOUNDATION_EXPORT const unsigned char MTInfoPanelKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MTInfoPanelKit/PublicHeader.h>
+#import <MTInfoPanelKit/MTInfoPanel.h>
+
